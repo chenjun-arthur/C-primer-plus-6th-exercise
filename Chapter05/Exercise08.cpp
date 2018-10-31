fb4b79027a24277b47e357aaa40043ed9d803905
CHEN Jun <chenjun-1988@qq.com>
2018-10-31 23:43:36 +0800

    add solution to Chapter05/Ex07

---
+++ Chapter05/Exercise08.cpp
@@ -0,0 +1,18 @@
+/*
+Chapter 5 Programming Exercises
+Write a program that uses an array of char and a loop to read one word at a time
+until the word done is entered.The program should then report the number of
+words entered (not counting done).A sample run could look like this:
+Enter words (to stop, type the word done):
+anteater birthday category dumpster
+envy finagle geometry done for sure
+You entered a total of 7 words.
+*/
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+	return 0;
+}
