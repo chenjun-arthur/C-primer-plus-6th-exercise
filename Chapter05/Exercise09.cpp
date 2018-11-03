67c5f53adc096d8acdf4c88cd019b4b9c7c30c94
CHEN Jun <chenjun-1988@qq.com>
2018-11-03 23:57:21 +0800

    Finish Chapter05 Exercises

--- Chapter05/Exercise09.cpp
+++ Chapter05/Exercise09.cpp
@@ -7,18 +7,21 @@ Enter words (to stop, type the word done):
 anteater birthday category dumpster
 envy finagle geometry done for sure
 You entered a total of 7 words.
+
+9. Write a program that matches the description of the program in Programming
+Exercise 8, but use a string class object instead of an array. Include the string
+header file and use a relational operator to make the comparison test.
 */
 #include <iostream>
-#include <cstring>
-const int ArSize = 20;
+#include <string>
 int main()
 {
     using namespace std;
-    char word[ArSize];
+    string word;
     int count = 0;
 
     cin >> word;
-    while (strcmp(word, "done")) {
+    while (word != "done") {
     	count++;
     	cin >> word;
     }
