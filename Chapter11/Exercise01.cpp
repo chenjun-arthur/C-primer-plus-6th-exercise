0ea9e66e87ad6c59b5678d42a2b210aaf205d479
chenjun <180097569@qq.com>
2019-02-08 19:31:21 +0800

    Ex1 pass

--- Chapter11/Exercise01.cpp
+++ Chapter11/Exercise01.cpp
@@ -16,3 +16,74 @@ the file.The file contents might look like this:
 	(m,a) = (107.212, -56.8194)
 	Average outward distance per step = 3.97081
 */
+// randwalk.cpp -- using the Vector class
+// compile with the vect.cpp file
+#include <iostream>
+#include <fstream>
+#include <cstdlib>      // rand(), srand() prototypes
+#include <ctime>        // time() prototype
+#include "vect.h"
+int main()
+{
+    using namespace std;
+    using VECTOR::Vector;
+    srand(time(0));     // seed random-number generator
+    double direction;
+    Vector step;
+    Vector result(0.0, 0.0);
+    unsigned long steps = 0;
+    double target;
+    double dstep;
+    cout << "Enter target distance (q to quit): ";
+    while (cin >> target)
+    {
+        cout << "Enter step length: ";
+        if (!(cin >> dstep))
+            break;
+
+        ofstream fout;
+        fout.open("thewalk.txt");
+        fout << "Target Distance: " << target << ", ";
+        fout << "Step Size: " << dstep << endl;
+
+        while (result.magval() < target)
+        {
+        	fout << steps << ": " << result << endl;
+
+            direction = rand() % 360;
+            step.reset(dstep, direction, Vector::POL);
+            result = result + step;
+            steps++;
+        }
+        cout << "After " << steps << " steps, the subject "
+            "has the following location:\n";
+        cout << result << endl;
+        result.polar_mode();
+        cout << " or\n" << result << endl;
+        cout << "Average outward distance per step = "
+            << result.magval()/steps << endl;
+
+        result.rect_mode();
+    	fout << steps << ": " << result << endl;
+        fout << "After " << steps << " steps, the subject "
+            "has the following location:\n";
+        fout << result << endl;
+        result.polar_mode();
+        fout << " or\n" << result << endl;
+        fout << "Average outward distance per step = "
+            << result.magval()/steps << endl;
+
+
+        steps = 0;
+        result.reset(0.0, 0.0);
+        cout << "Enter target distance (q to quit): ";
+    }
+    cout << "Bye!\n";
+/* keep window open
+    cin.clear();
+    while (cin.get() != '\n')
+        continue;
+    cin.get();
+*/
+    return 0; 
+}
