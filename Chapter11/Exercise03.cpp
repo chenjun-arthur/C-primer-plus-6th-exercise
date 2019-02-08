7b0be674977f88385c0fcfed41493f5958534fd9
chenjun <180097569@qq.com>
2019-02-08 17:35:51 +0800

    Chapter11 start

---
+++ Chapter11/Exercise03.cpp
@@ -0,0 +1,5 @@
+/*Chapter 11 Programming Exercises
+3. Modify Listing 11.15 so that instead of reporting the results of a single trial for a
+particular target/step combination, it reports the highest, lowest, and average number
+of steps for N trials, where N is an integer entered by the user.
+*/
