288d8f6a305ea27dbd231483d06a7fd7e74eaa16
chenjun <180097569@qq.com>
2019-02-08 22:01:31 +0800

    Ex04 pass

--- Chapter11/Exercise05.cpp
+++ Chapter11/Exercise05.cpp
@@ -6,3 +6,43 @@ show_stn() and show_lbs() methods. Overload the addition, subtraction, and
 multiplication operators so that one can add, subtract, and multiply Stonewt values.
 Test your class with a short program that uses all the class methods and friends.
 */
+// stone.cpp -- user-defined conversions
+// compile with stonewt.cpp
+#include <iostream>
+using std::cout;
+#include "stonewt.h"
+void display(const Stonewt & st, int n);
+int main()
+{
+    Stonewt incognito = 275; // uses constructor to initialize
+    Stonewt wolfe(285.7);    // same as Stonewt wolfe = 285.7;
+    Stonewt taft(21, 8);
+
+    cout << "The celebrity weighed ";
+    incognito.show_stn();
+    cout << "The detective weighed ";
+    wolfe.show_stn();
+    cout << "The President weighed ";
+    taft.show_lbs();
+    incognito = 276.8;      // uses constructor for conversion
+    taft = 325;             // same as taft = Stonewt(325);
+    cout << "After dinner, the celebrity weighed ";
+    incognito.show_stn();
+    cout << "After dinner, the President weighed ";
+    taft.show_lbs();
+    display(taft, 2);
+    cout << "The wrestler weighed even more.\n";
+    display(422, 2);
+    cout << "No stone left unearned\n";
+    // std::cin.get();
+    return 0;
+}
+
+void display(const Stonewt & st, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Wow! ";
+        st.show_stn();
+    }
+}
